58c182aeb2bb410bbf3d62aa904953bab8a00ada
yashbitcode <yashkumar.sre.177@gmail.com>
2025-04-02 14:14:00 +0530

    VSF 3

--- api/data.js
+++ api/data.js
@@ -2,8 +2,10 @@ const handler = async (req, res) => {
     try {
         const {url} = req.body;
         const apiKey = process.env.API_KEY;
+
+        // console.log(url + apiKey)
         
-        const response = await fetch(url + apiKey);
+        const response = await fetch(url + "api_key=" + apiKey);
         const data = await response.json();
 
         res.status(200).json(data);
